951221f512ee23214d259d24d9978d12f1176494
Bartosz Lewandowski <112126@student.upwr.edu.pl>
2022-07-20 23:09:13 +0200

    small upgrade

--- src_new_way/CNV_generator.py
+++ src_new_way/CNV_generator.py
@@ -52,7 +52,46 @@ class CNVGenerator:
         with open(self.pathout + "chrs.bed", "w") as chr_file:
             chr_info = "\t".join([fasta_file.id,"1",str(len_fasta)]) + "\n"
             chr_file.write(chr_info)
+    
+    def generate_freq(self,):
+        NUMBER_OF_CHROMOSOMES = 1
+        no_chrs = range(1, NUMBER_OF_CHROMOSOMES+1)
+        chr_freq = {}
+        for i in no_chrs:
+            chr_freq[i] = round(i/1,3)
+
+        for cnv_type in ["del","dup"]:
+            out = open(self.pathOut + cnv_type + "3.bed","w")
+            for line in open(self.pathOut + cnv_type + "2.bed"):
+                if cnv_type == "del":
+                    #1 bo dla jednego (i pierwszego) chromosomu chcemy zrobić modyfikację
+                    num = random.randint(1,1)
+                    out.write(line.rstrip() + "\tdel\t" + str(chr_freq[num]) + "\t0\n")
+                elif cnv_type == "dup":
+                    num = random.randint(1,1)
+                    count = np.random.choice([2,3,4,5,6,7,8,9,10], 1, p=[0.5, 0.1, 0.1, 0.05, 0.05,0.05,0.05,0.05,0.05])[0]
+                    freqs = num/1
+                    cp = (count*freqs) + ((1-freqs) * 1)
+                    while cp == 1.0:
+                        num = random.randint(1,1)
+                        count = np.random.choice([2,3,4,5,6,7,8,9,10], 1, p=[0.5, 0.1, 0.1, 0.05, 0.05,0.05,0.05,0.05,0.05])[0]
+                    out.write(line.rstrip() + "\tdup\t" + str(chr_freq[num]) + "\t" + str(count) + "\n")
+            out.close()
+            for j in chr_freq:
+                out = open(self.pathOut + cnv_type + "." + str(j) + ".bed","w")
+                for line in open(self.pathOut + cnv_type + "3.bed"):
+                    if float(line.split()[4]) >= chr_freq[j]:
+                        out.write(line)
+                out.close()
 
 if __name__ == "__main__":
     cnv = CNVGenerator("fasta.fa")
     cnv.generate_cnv()
+
+
+
+"""
+PO WSZYSTKIM ZOSTAJE PLIK DEL.1.bed, dup.1.bed, total.1.bed
+gdzie tak naprawdę tylko total1.bed jest używany w simChr.py - więc on jest tylko wykorzystywany
+mogę tę strukturę total1.bed wykorzystać jako dataframe i wtedy go zapisać w pamięci, zamiast zapisywać w pliku
+"""
