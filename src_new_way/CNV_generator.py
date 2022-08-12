517cc53e1d2bc08d6e61a8957a42205759db315d
Bartosz-Lewandowski <b.lewandowski05@gmail.com>
2022-08-12 12:06:46 +0200

    Update CNV-generator Generate CNV with step 50bp

--- src_new_way/CNV_generator.py
+++ src_new_way/CNV_generator.py
@@ -46,13 +46,23 @@ class CNVGenerator:
         dup, dele, chr_info = self.clean_bed_dup_del()
         dup_freq, dele_freq = self.generate_freq(dup, dele)
         normal = self.bedfile_with_normal_seq(dup_freq, dele_freq, chr_info)
-        total = self.create_total_bed(dup_freq, dele_freq, normal)
+        total, total_bedtool = self.create_total_bed(dup_freq, dele_freq, normal)
+        self.create_total_windows(total_bedtool, chr_info)
         return total
+
+    def create_total_windows(self, total_bedtool, chr_info):
+        chr_info_bed = CNVGenerator.__BedFormat_to_BedTool(chr_info)
+        BedTool.window_maker(chr_info_bed, b=total_bedtool, w=50, s=50, i="src").saveas(
+            f"{self.pathout}total.bed"
+        )
 
     def clean_bed_dup_del(self) -> tuple[BedTool, BedTool, np.array]:
         dup, dele, chr_info = self._create_bed_with_coords()
+        print("Sortking and merging dele")
         dele_merged = self._cnv_sort_and_merge(dele)
+        print("sorting and merging dup")
         dup_merged = self._cnv_sort_and_merge(dup)
+        print("INTERSECTING")
         dup_intersect, dele_intersect = self._cnv_intersect(dup_merged, dele_merged)
         return dup_intersect, dele_intersect, chr_info
 
@@ -66,17 +76,23 @@ class CNVGenerator:
         Returns:
             tuple[np.array, np.array]: Returns same dup and dele object, but with frequencies
         """
+        print("generating dup freq")
         dup_with_freq: np.array = self._create_dup_freqs(dup)
+        print("generating dele freq")
         dele_with_freq: np.array = self._create_dele_freqs(dele)
         return dup_with_freq, dele_with_freq
 
     def bedfile_with_normal_seq(
         self, dup: np.array, dele: np.array, chr_info: np.array
     ) -> np.array:
+        print("creating normal seq")
         chr_info_bed = CNVGenerator.__BedFormat_to_BedTool(chr_info)
-        normal = BedTool().window_maker(chr_info_bed, w=5)
+        print("window maker")
+        normal = BedTool().window_maker(chr_info_bed, w=50)
+        print("dup dele")
         dup_bedtools = CNVGenerator.__BedFormat_to_BedTool(dup).saveas(f"{self.pathout}dup.bed")
         dele_bedtools = CNVGenerator.__BedFormat_to_BedTool(dele).saveas(f"{self.pathout}del.bed")
+        print("normal 2")
         normal2 = (
             normal.intersect(dup_bedtools, v=True, wa=True)
             .intersect(dele_bedtools, v=True, wa=True)
@@ -91,12 +107,11 @@ class CNVGenerator:
         return out_normal
 
     def create_total_bed(self, dup: np.array, dele: np.array, normal: np.array) -> np.array:
+        print("creating total file")
         total = np.concatenate((dup, dele, normal), axis=0)
-        total_sorted = (
-            CNVGenerator.__BedFormat_to_BedTool(total).sort().saveas(f"{self.pathout}total.bed")
-        )
+        total_sorted = CNVGenerator.__BedFormat_to_BedTool(total).sort()
         total_BedFormat = CNVGenerator.__BedTool_to_BedFormat(total_sorted)
-        return total_BedFormat
+        return total_BedFormat, total_sorted
 
     def _create_dele_freqs(self, dele: np.array) -> np.array:
         out = np.array([])
@@ -135,6 +150,7 @@ class CNVGenerator:
         dup = np.array([])
         dele = np.array([])
         chr_info = np.array([])
+        print("Creating bed objects with coordinates of cnv")
         for chr in tqdm(SeqIO.parse(open(self.fasta_file), "fasta"), total=self.num_of_chroms):
             dup = np.append(dup, np.array([self.__generate_coords(chr)]))
             dele = np.append(dele, np.array([self.__generate_coords(chr)]))
@@ -145,7 +161,7 @@ class CNVGenerator:
     def __generate_coords(self, fasta_file) -> np.array:
         len_fasta: int = len(fasta_file.seq)
         lenghts: list = []
-        max_cnv_length = 100000
+        max_cnv_length = 1000
         cnv_count = int(
             (len_fasta / max_cnv_length) / 2
         )  # number of cnv that can fit in data devided by two because there are two types of cnv (duplications and deletions)
