420bfeedf603ecf5976a06bbf78ab82b7933fbc4
Bartosz-Lewandowski <b.lewandowski06@gmail.com>
2022-08-12 12:57:02 +0200

    update combining ref genome

--- src_new_way/combine_ref_gens.sh
+++ src_new_way/combine_ref_gens.sh
@@ -1,6 +1,8 @@
-for f in reference_genome/*.gz; do
-  zcat "$f" | head -n 5000
-done | gzip -c > reference_genome/ref_genome_short.fa.gz
-
+#!/bin/bash
+rm -r reference_genome/ref_genome.fa
+for f in reference_genome/*.gz
+do
+  echo "Combining $f file..."
+  zcat < $f | head -n 10000 >> reference_genome/ref_genome.fa
+done 
 rm -r reference_genome/Sus_scrofa.Sscrofa11.1.dna*.fa.gz
-gunzip reference_genome/ref_genome_short.fa.gz
