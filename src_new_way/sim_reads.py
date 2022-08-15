a034ba8319c69f0745a72abd3c9eb98ecdd6712e
Bartosz-Lewandowski <b.lewandowski06@gmail.com>
2022-08-15 15:40:20 +0200

    change os module to subprocess in sim reads

--- src_new_way/sim_reads.py
+++ src_new_way/sim_reads.py
@@ -1,4 +1,5 @@
 import os
+from subprocess import call
 
 import numpy as np
 from Bio import SeqIO
@@ -29,15 +30,20 @@ class SimReads:
             os.makedirs(self.pathout)
 
     def sim_reads_genome(self) -> tuple[str, str]:
-        chrs = np.array([len(fasta.seq) for fasta in SeqIO.parse(open(self.fasta_file), "fasta")])
+        chrs = np.array(
+            [len(fasta.seq) for fasta in SeqIO.parse(open(self.fasta_file), "fasta")]
+        )
         chrs_len = np.sum(chrs)
         N = self._calc_N_reads(chrs_len)
         return self.sim_reads_with_InSilicoSeq(N)
 
     def sim_reads_with_InSilicoSeq(self, N: int) -> tuple[str, str]:
         command = f"iss generate --model {self.model} --genomes {self.fasta_file} --n_reads {N} --cpus {self.cpu} --output {self.pathout}{self.cov}"
-        os.system(command)
-        return f"{self.pathout}{self.cov}_R1.fastq", f"{self.pathout}{self.cov}_R2.fastq"
+        call(command, shell=True)
+        return (
+            f"{self.pathout}{self.cov}_R1.fastq",
+            f"{self.pathout}{self.cov}_R2.fastq",
+        )
 
     def _get_read_length(self) -> int:
         match self.model:
