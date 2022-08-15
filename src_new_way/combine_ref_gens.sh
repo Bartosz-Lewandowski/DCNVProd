f98f1c70295cfc317e593ab58fa7ceacb725cc17
Bartosz-Lewandowski <b.lewandowski06@gmail.com>
2022-08-15 17:14:29 +0200

    take all ref genome, not short

--- src_new_way/combine_ref_gens.sh
+++ src_new_way/combine_ref_gens.sh
@@ -3,6 +3,6 @@ rm -r reference_genome/ref_genome.fa
 for f in reference_genome/*.gz
 do
   echo "Combining $f file..."
-  zcat < $f | head -n 10000 >> reference_genome/ref_genome.fa
+  zcat < $f >> reference_genome/ref_genome.fa
 done 
 rm -r reference_genome/Sus_scrofa.Sscrofa11.1.dna*.fa.gz
