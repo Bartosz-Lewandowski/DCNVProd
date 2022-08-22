33abd4862e92417a378fd28aa941b967416fd0ed
Bartosz-Lewandowski <b.lewandowski06@gmail.com>
2022-08-22 18:07:42 +0200

    update argparser

--- src_new_way/argparser.py
+++ src_new_way/argparser.py
@@ -1,5 +1,27 @@
 import argparse
 
+CHRS = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "all",
+]
+
 
 def arg_parser():
     parser = argparse.ArgumentParser()
@@ -44,5 +66,40 @@ def arg_parser():
         required=False,
         default=10,
     )
-
+    parser.add_argument(
+        "-w",
+        "--window_size",
+        dest="window_size",
+        help="Genomic window size",
+        required=False,
+        default=50,
+    )
+    parser.add_argument(
+        "-chr",
+        "--chromosomes",
+        dest="chrs",
+        help=f"Pick chromosomes to build model on. Chromosomes to pick: {CHRS} or 'all' for all of them",
+        nargs="+",
+        choices=CHRS,
+        required=False,
+        default="1",
+        action="store",
+    )
+    parser.add_argument(
+        "-new",
+        "--new_data",
+        help="Create new simulated data and compute all features again. By default data is taken from GCP bucket.",
+        dest="new",
+        action="store_true",
+        default=False,
+        required=False,
+    )
+    parser.add_argument(
+        "-EDA",
+        help="Perform EDA and save plots.",
+        dest="EDA",
+        action="store_true",
+        default=False,
+        required=False,
+    )
     return parser
