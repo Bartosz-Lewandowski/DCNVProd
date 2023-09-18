3b00d30cefbd4897d03ea3d8074a1b8ce39c4567
Bartosz-Lewandowski <b.lewandowski06@gmail.com>
2023-09-18 20:40:05 +0200

    add tests for clean_bed_dup_del

---
+++ tests/test_clean_bed_dup_del.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pytest
+from pybedtools import BedTool
+
+from src.cnv_generator import CNVGenerator
+
+
+@pytest.fixture
+def cnv_generator(sample_fasta_file):
+    cnv_gen = CNVGenerator(sample_fasta_file, 50, 75, 10, 0.7)
+    return cnv_gen
+
+
+def test_clean_bed_dup_del(cnv_generator):
+    result = cnv_generator._clean_bed_dup_del()
+    assert len(result) == 3
+    assert isinstance(result[0], BedTool)
+    assert isinstance(result[1], BedTool)
+    assert isinstance(result[2], np.ndarray)
